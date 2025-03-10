788303b8bc6d0750670eedfe49938f2bd25bdaee
Ghilas Amarouche <6194975@claurendeau.qc.ca>
2025-03-10 00:20:13 -0400

    ctrlAtlO

--- src/main/java/ca/cal/tp2/Repository/EmprunteurRepository.java
+++ src/main/java/ca/cal/tp2/Repository/EmprunteurRepository.java
@@ -2,9 +2,6 @@ package ca.cal.tp2.Repository;
 
 import ca.cal.tp2.modele.Emprunteur;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface EmprunteurRepository extends Repository<Emprunteur>{
 
 }
